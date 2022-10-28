fe520196a542371a03085dfea53adc919a9d19bf
zjx <1193028309@qq.com>
2022-10-28 14:52:26 +0800

    '小赵第四次修改'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 var p1 = document.getElementsByTagName("p")[0];
-p1.addEventListener("click", function () {
-  p1.innerHTML = "js更改之后的内容 小凯";
-  p1.style.backgroundColor = "yellow";
-  p1.style.color = "green";
+p1.addEventListener("click", function() {
+    p1.innerHTML = "js更改之后的内容 小凯";
+    p1.style.backgroundColor = "yellow";
+    p1.style.color = "green";
+});
+var p4 = document.getElementsByTagName("p")[3];
+p4.addEventListener("click", function() {
+    p4.innerHTML = "js更改之后的内容 小赵";
+    p4.style.backgroundColor = "red";
+    p4.style.color = "skyblue";
 });
