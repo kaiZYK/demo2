d8cd7890d1d9fb8cb38ce14f48765a9638feed27
zhaoyukai <1020849724@qq.com>
2022-10-28 14:38:31 +0800

    第二次修改 小凯

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ p1.addEventListener("click", function () {
   p1.style.backgroundColor = "yellow";
   p1.style.color = "green";
 });
+var span1 = document.getElementsByTagName("span")[0];
+span1.addEventListener("click", function () {
+  span1.innerHTML = "js更改之后的内容 小凯";
+  span1.style.backgroundColor = "yellow";
+  span1.style.color = "green";
+});
