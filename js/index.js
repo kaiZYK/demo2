23d80731a899180e100a13d29285be514737b2b0
xiaokai <1020849724@qq.com>
2022-10-28 15:11:24 +0800

    第四次修改 小凯

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 var p1 = document.getElementsByTagName("p")[0];
-p1.addEventListener("click", function () {
-  p1.innerHTML = "js更改之后的内容 小凯";
-  p1.style.backgroundColor = "yellow";
-  p1.style.color = "green";
+p1.addEventListener("click", function() {
+    p1.innerHTML = "js更改之后的内容 小凯";
+    p1.style.backgroundColor = "yellow";
+    p1.style.color = "green";
 });
 var span1 = document.getElementsByTagName("span")[0];
-span1.addEventListener("click", function () {
-  span1.innerHTML = "js更改之后的内容 小凯";
-  span1.style.backgroundColor = "yellow";
-  span1.style.color = "green";
+span1.addEventListener("click", function() {
+    span1.innerHTML = "js更改之后的内容 小凯";
+    span1.style.backgroundColor = "yellow";
+    span1.style.color = "green";
+});
+var p4 = document.getElementsByTagName("p")[3];
+p4.addEventListener("click", function() {
+    p4.innerHTML = "js更改之后的内容 小赵";
+    p4.style.backgroundColor = "red";
+    p4.style.color = "skyblue";
 });
